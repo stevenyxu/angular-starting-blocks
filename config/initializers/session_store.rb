662f0bcdb8a0a717823cac2cd538f7a9dd983cc6
Steven Xu <steven@stevenxu.ca>
2013-05-26 20:26:26 -0400

    updating dependencies to rails 4 rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-JavascriptWebClient::Application.config.session_store :encrypted_cookie_store, key: '_javascript_web_client_session'
+JavascriptWebClient::Application.config.session_store :cookie_store, key: '_javascript_web_client_session'
